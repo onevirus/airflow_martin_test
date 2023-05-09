5220fd1c657d59b3e94bd21c4ed78f157f6c0a37
Ilgyun Jeong <onevirus@gmail.com>
2023-05-09 16:11:28 +0900

    Test

---
+++ dags/noti.py
@@ -0,0 +1,15 @@
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+with DAG(
+    “noti”,
+    start_date=datetime(2023, 5, 5),
+    schedule=timedelta(hours=1),
+) as dag:
+    t1 = BashOperator(
+        task_id="print_date",
+        bash_command="date",
+    )
